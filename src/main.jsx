393092f3689fd622d45f6969458c13e2eb2293fc
Melnyk Viktoriia <vikavmelnyk@gmail.com>
2023-07-09 23:26:19 +0300

    fix

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter basename="/test-task-tweetcards">
+    <BrowserRouter basename="/tweet-cards">
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
